a70ec983fa5b05c9ab559cfd080545b31f937917
bhaskrr <74664799+bhaskrr@users.noreply.github.com>
2024-01-21 21:27:54 +0530

    added the function to fetch the data

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const API_KEY = '4e14dde50d68efecdae9770d6eb25659';
+const loc = 'Delhi';
+
+async function getWeatherData(){
+    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${loc}&units=metric&appid=${API_KEY}`);
+    const weatherInfo = await response.json();
+    console.log(weatherInfo);
+}
+
+getWeatherData();
